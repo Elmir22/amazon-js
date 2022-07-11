504eccf982c79ff9b8e4569148f5a474e598e84a
Elmir22 <elmirabbasov002@gmail.com>
2022-07-11 19:26:49 +0000

    app.js end

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 "use strict";
 $(document).ready(function () {
 
-
     $('.btn').click(function (e) {
         e.preventDefault();
-        let k = v => {
-            for (let i = 0; i < v; i++) { $('.box').append(`<h3>${v}</h3>`) };
+        if ($.trim($('input').val()) == '') {
+            $('input').val();
+        } else {
+            let k = v => {
+                for (let i = 0; i < v; i++) { $('.box').append(`<h3>${v}</h3>`) };
+            }
+            let z = e => Number(e) ? k(e) : $('.box').append(`<h3>${e}</h3>`);
+            z($('input').val());
         }
-        let z = e => Number(e) ? k(e) : $('.box').append(`<h3>${e}</h3>`)
-            ;
-        z($('input').val());
-        $('input').val('')
-
+        $('input').val('') 
     });
-
 
 });

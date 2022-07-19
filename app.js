f67fbdb55f0c870f4629a41d0dd0bf05f3d9e886
Elmir22 <elmirabbasov002@gmail.com>
2022-07-19 18:49:06 +0000

    html sass js end

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 "use strict";
 $(document).ready(function () {
 
+    // $('.btn').click(function (e) {
+    //     e.preventDefault();
+    //     if ($.trim($('input').val()) == '') {
+    //         $('input').val();
+    //     } else {
+    //         let k = v => {
+    //             for (let i = 0; i < v; i++) { $('.box').append(`<h3>${v}</h3>`) };
+    //         }
+    //         let z = e => Number(e) ? k(e) : $('.box').append(`<h3>${e}</h3>`);
+    //         z($('input').val());
+    //     }
+    //     $('input').val('') 
+    // })
+
+
     $('.btn').click(function (e) {
         e.preventDefault();
-        if ($.trim($('input').val()) == '') {
-            $('input').val();
-        } else {
+        if (!$.trim($('input').val()) == '') {
             let k = v => {
                 for (let i = 0; i < v; i++) { $('.box').append(`<h3>${v}</h3>`) };
             }
